61d2b87a519c59fa40401d7d9ee48d112ec6706c
Lopevida <200292448@BA-B225-29.student.georgianc.on.ca>
2015-10-07 19:11:04 -0400

    second

--- resume/routes/about.js
+++ resume/routes/about.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Resume' });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About page' });
 });
 
 module.exports = router;
