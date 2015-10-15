6efd61bce7aa39fee9b288de38305b55c3df1f8f
Lopevida <200292448@BA-B225-29.student.georgianc.on.ca>
2015-10-14 21:18:37 -0400

    edited all ages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Resume' });
+  res.render('index', { title: 'Yuelin' });
 });
+router.get('/index', function (req, res, next) {
+    res.render('index', { title: 'Yuelin' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('about', { title: 'About' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('contact', { title: 'Contact' });
+});
+router.get('/service', function (req, res, next) {
+    res.render('service', { title: 'Service' });
+});
+router.get('/project', function (req, res, next) {
+    res.render('project', { title: 'Project' });
+});
+
+
+
 
 module.exports = router;
